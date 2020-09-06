77040d8af443967ae872ee792f117fb2357834e0
野泽 <ziniu.wzn@alibaba-inc.com>
2020-09-06 12:10:55 +0800

    update

--- Testing/BN_testing.py
+++ Testing/BN_testing.py
@@ -1,8 +1,10 @@
 from Evaluation.cardinality_estimation import parse_query_single_table
 from Evaluation.parse_query_imdb import prepare_join_queries
+from Models.BN_ensemble_model import BN_ensemble
 from time import perf_counter
 import numpy as np
 import pickle
+import time
 
 def evaluate_cardinality_single_table(model_path, query_path, infer_algo):
     # load BN
@@ -53,8 +55,42 @@ def evaluate_cardinality_single_table(model_path, query_path, infer_algo):
 
 
 def evaluate_cardinality_imdb(schema, model_path, query_path, infer_algo, learning_algo, max_parents):
-    ensemble_location = "/home/yuxing.hyx/repository/imdb-benchmark/spn_ensembles/ensemble_relationships_imdb-light_10000000.pkl"
-    pairwise_rdc_path = "/home/yuxing.hyx/repository/imdb-benchmark/spn_ensembles/pairwise_rdc.pkl"
+    ensemble_location = "../Benchmark/IMDB/ensemble_loader.pkl"
+    pairwise_rdc_path = "../Benchmark/IMDB/pairwise_rdc.pkl"
     parsed_queries, true = prepare_join_queries(ensemble_location, pairwise_rdc_path, query_path,
                                                 join_3_rdc_based=False, true_card_exist=True)
 
+    with open(query_path, "rb") as f:
+        real_query = f.readlines()
+    bn_ensemble = BN_ensemble(schema)
+    for i in range(5):
+        with open(f"{model_path}/{i}_{learning_algo}_{max_parents}.pkl", "rb") as f:
+            bn = pickle.load(f)
+            bn.infer_algo = infer_algo
+            bn.init_inference_method()
+        bn_ensemble.bns[i] = bn
+
+    queries = bn_ensemble.parse_query_all(parsed_queries)
+
+    latency = []
+    q_errors = []
+    for i, q in enumerate(queries):
+        tic = time.time()
+        try:
+            pred = bn_ensemble.cardinality(q)
+        except:
+            # this query itself is invalid or it is not recognizable by the learnt BN
+            continue
+        latency.append(time.time() - tic)
+        if pred is None or pred <= 1:
+            pred = 1
+        error = max(pred / true[i], true[i] / pred)
+        print(f"predicting query no {i}: {real_query[i]} \n")
+        print(f"true cardinality {true[i]}, predicted {pred} with q-error {error}")
+        q_errors.append(error)
+    print("=====================================================================================")
+    for i in [50, 90, 95, 99, 100]:
+        print(f"q-error {i}% percentile is {np.percentile(q_errors, i)}")
+    print(f"average latency is {np.mean(latency)} ms")
+
+    return latency, q_errors
